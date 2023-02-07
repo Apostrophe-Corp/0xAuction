7c39fcd59feaa9d4ed2d487c6296e228591b2862
goonerlabs <goonerlabs@gmail.com>
2023-02-07 21:34:49 +0100

    update testnet.js

--- Algorand-NFT-SDK/javaScript-sdk/testnet.js
+++ Algorand-NFT-SDK/javaScript-sdk/testnet.js
@@ -1,92 +1,231 @@
-const { generateAccount } = require("algosdk");
-const algosdk = require("algosdk");
-const crypto = require("crypto");
-const fs = require("fs");
-const { createInflate } = require("zlib");
+const { generateAccount } = require('algosdk')
+const algosdk = require('algosdk')
+const crypto = require('crypto')
+const fs = require('fs')
+const { createInflate } = require('zlib')
 
-const algodTestnetServer = "https://testnet-api.algonode.cloud";
-const indexerTestnetServer = "https://testnet-idx.algonode.cloud"
-const algodBetanetServer = "https://betanet-api.algonode.cloud";
-const indexerBetanetServer = "https://betanet-idx.algonode.cloud"
-const algodMainetServer = "https://mainnet-api.algonode.cloud";
-const indexerMainnetServer = "https://mainnet-idx.algonode.cloud"
+const algodTestnetServer = 'https://testnet-api.algonode.cloud'
+const indexerTestnetServer = 'https://testnet-api.algonode.cloud'
+const algodBetanetServer = 'https://betanet-api.algonode.cloud'
+const indexerBetanetServer = 'https://betanet-idx.algonode.cloud'
+const algodMainetServer = 'https://mainnet-api.algonode.cloud'
+const indexerMainnetServer = 'https://mainnet-idx.algonode.cloud'
 const algodPort = 443;
 const indexerPort = 443;
-const algodToken = "";
-const indexerToken = ""
+const algodToken = ''
+const indexerToken = ''
 
-adminAddress = "W4BERQ52RZILAKXNJJ6X5FNY3ASIAK3OV6KWX7DRTLKHXE7HNNGCO5OVUA"
-adminKey = "3d43u0uywHWdackLRDwgZA8cBqpb2FxUJECl36fhcvS3AkjDuo5QsCrtSn1+lbjYJIArbq+Va/xxmtR7k+drTA=="
+adminAddress = 'W4BERQ52RZILAKXNJJ6X5FNY3ASIAK3OV6KWX7DRTLKHXE7HNNGCO5OVUA'
+adminKey =
+	'3d43u0uywHWdackLRDwgZA8cBqpb2FxUJECl36fhcvS3AkjDuo5QsCrtSn1+lbjYJIArbq+Va/xxmtR7k+drTA=='
 
-const algodClient = new algosdk.Algodv2(algodToken, algodTestnetServer, algodPort)
-const indexerClient = new algosdk.Indexer(indexerToken, indexerTestnetServer, indexerPort);
+const algodClient = new algosdk.Algodv2(
+	algodToken,
+	algodTestnetServer,
+	algodPort
+)
+const indexerClient = new algosdk.Indexer(
+	indexerToken,
+	indexerTestnetServer,
+	indexerPort
+)
 
 const createAccount = function () {
-  try {
-    const myaccount = algosdk.generateAccount();
-    console.log("Account Address = " + myaccount.addr);
-    let account_mnemonic = algosdk.secretKeyToMnemonic(myaccount.sk);
-    console.log("Account Mnemonic = " + account_mnemonic);
-    console.log("Account created. Please store Mnemonic and address");
-    console.log("Add funds to account using the TestNet Dispenser: ");
-    console.log(
-      "https://dispenser.testnet.aws.algodev.network/?account=" + myaccount.addr
-    );
+	try {
+		const myaccount = algosdk.generateAccount()
+		console.log('Account Address = ' + myaccount.addr)
+		let account_mnemonic = algosdk.secretKeyToMnemonic(myaccount.sk)
+		console.log('Account Mnemonic = ' + account_mnemonic)
+		console.log('Account created. Please store Mnemonic and address')
+		console.log('Add funds to account using the TestNet Dispenser: ')
+		console.log(
+			'https://dispenser.testnet.aws.algodev.network/?account=' + myaccount.addr
+		)
 
-    return myaccount;
-  } catch (err) {
-    console.log("err", err);
-  }
-};
+		return myaccount
+	} catch (err) {
+		console.log('err', err)
+	}
+}
 
+async function checkOptIn(address, assetId) {
+    const accountInfo = await tokenClient.accountInformation(address).do();
+    const assets = accountInfo.assets;
 
-async function createNft (name, symbol, address, imageUrl, reserve, clawback, freeze) {
-
- 
- // todo hash url and log it to the console
-
- if (address === undefined) {
-  generateAccount();    
-  } 
-
- const sp = await algodClient.getTransactionParams().do();
-
- // todo create if statement to check if reserve and if reserve, build metadata hash else empty string 
- const reserveHash = "";
-
- const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
-  // todo may change url p
-  assetMetadataHash: imageUrl,
-  assetName: name,
-  assetURL: imageUrl,
-  clawback: clawback ? clawback : "",
-  decimals: 0,
-  defaultFrozen: false,
-  freeze: freeze ? freeze : "",
-  from: adminAddress,
-  manager: adminAddress,
-  reserve: reserveHash,
-  suggestedParams: sp,
-  total: 1,
-  unitName: symbol,
- });
-
- const rawSignedTxn = txn.signTxn(adminKey);
-
- let tx = (await algodClient.sendRawTransaction(rawSignedTxn).do());
- console.log("Transaction : " + tx.txId);
- let assetID = null;
- // wait for transaction to be confirmed
- // await waitForConfirmation(algodClient, tx.txId);
- // Get the new asset's information from the creator account
- let ptx = await algodClient.pendingTransactionInformation(tx.txId).do();
- assetID = ptx["asset-index"];
- console.log("AssetID = " + assetID);
-
- // todo verify optin
-
- // todo transfer nft
+    let optInStatus = false;
+    for (const asset of assets) {
+        if (asset.asset_id === parseInt(assetId)) {
+            optInStatus = true;
+            break;
+        }
+    }
+	return optInStatus;
 
 }
 
-// todo update nft function 
+async function claimNFT(address, assetId) {
+	const isOptIn = await checkOptIn(address, assetId);
+	if (!isOptIn) {
+	  console.log(`Asset with Id: ${assetId} has not yet been opted into by Wallet: ${address}. Please opt-in`);
+	  return false;
+	}
+  
+	const params = algodClient.getTransactionParams();
+	const txn = algosdk.makeAssetTransferTxn(
+	  address, 
+	  [{assetId: assetId, amount: 1}], 
+	  params
+	);
+	const signedTxn = txn.sign(adminKey);
+	const tx = await algodClient.sendRawTransaction(signedTxn).do();
+	console.log(`Successfully sent transaction with txID: ${tx.txId}`);
+  
+	const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
+	console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
+	console.log(`Transaction ID: ${tx.txId}`);
+  
+	return tx.txId;
+}
+  
+
+  
+
+const verifyOptInBeforeTransfer = async (address, asset) => {
+	new Promise((resolve) => {
+		let waiter = undefined;
+		waiter = setInterval(async () => {
+			const hasOptIn = await checkOptIn(address, asset); // true || false
+			if (hasOptIn) {
+				clearInterval(waiter)
+				waiter = undefined
+				resolve()
+			}
+		}, 5000)
+	})
+		.then(async () => {
+			await claimNFT(address, asset);
+		})
+		.catch((error) => {
+			console.log('err', err);
+		})
+}
+
+async function createNft({
+	name = undefined,
+	symbol = undefined,
+	address = undefined,
+	url = undefined,
+	clawback = "",
+	freeze = ""
+} = {}) {
+	if (name === undefined || symbol === undefined || url === undefined){
+		console.log("Argument missing required parameter");
+		return;
+	}
+
+	if (address === undefined) {
+		generateAccount()
+	}
+
+	const sp = await algodClient.getTransactionParams().do()
+
+	// todo create if statement to check if reserve and if reserve, build metadata hash else empty string
+	const hash = crypto.createHash('sha256');
+	hash.update(url)
+	const digest = hash.digest()
+	const hexDigest = digest.toString('hex');
+ console.log(`Your metadata hash: ${hexDigest}`);
+
+
+	const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+		// todo may change url p
+		assetMetadataHash: "",
+		assetName: name,
+		assetURL: url,
+		clawback: clawback,
+		decimals: 0,
+		defaultFrozen: false,
+		freeze: freeze,
+		from: adminAddress,
+		manager: adminAddress,
+		reserve: "",
+		suggestedParams: sp,
+		total: 1,
+		unitName: symbol,
+	})
+
+	const rawSignedTxn = txn.signTxn(adminKey)
+
+	let tx = await algodClient.sendRawTransaction(rawSignedTxn).do()
+	console.log('Transaction : ' + tx.txId)
+	let assetID = null
+	// wait for transaction to be confirmed
+	
+	try{
+	const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
+
+	// Get the new asset's information from the creator account
+	let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
+	assetID = ptx['asset-index']
+
+	console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
+    console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`);
+
+	} catch (err) {
+    console.error(err);
+    return false;
+ 	}
+	verifyOptInBeforeTransfer(address, assetID);
+
+}
+
+// todo update nft function
+async function updateNFT({assetId = undefined, reserve = undefined, clawback = "", freeze = ""} = {}) {
+
+	if (assetId === undefined || reserve === undefined){
+		console.log("Argument missing required parameter");
+		return
+	}
+
+    const suggestedParams = await algosdk.algod.getTransactionParams();
+    
+    const txn = algosdk.makeAssetConfigTransaction(
+      adminAddress,
+      {
+        index: assetId,
+        manager: adminAddress,
+        reserve: reserve,
+        freeze: freeze,
+        clawback: clawback,
+        defaultFrozen: false
+      },
+      suggestedParams
+    );
+    
+    const rawSignedTxn = txn.signTxn(adminKey)
+
+
+    let tx = await algodClient.sendRawTransaction(rawSignedTxn).do()
+    console.log('Transaction : ' + tx.txId)
+    let assetID = null
+    // wait for transaction to be confirmed
+   
+    try{
+    const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
+
+
+    // Get the new asset's information from the creator account
+    let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
+    assetID = ptx['asset-index']
+
+
+    console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
+    console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`);
+
+
+    } catch (err) {
+    console.error(err);
+    return false;
+    }
+}
+createNft({name: "labi", symbol: "LA", url: "https://bit.ly/3iLVoA3#i", address: "BKULWP4WWNEFJRAUEZJD4RCWQ6G4XVH24MZINYOHB76SF2MTMFEOM5CABY"}) 
--- Algorand-NFT-SDK/python-algorand-sdk/py_sdk/testnet.py
+++ Algorand-NFT-SDK/python-algorand-sdk/py_sdk/testnet.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import time
 from algosdk.v2client import algod, indexer
 from algosdk import transaction, account, mnemonic
 
@@ -13,10 +14,67 @@ admin_key = "3d43u0uywHWdackLRDwgZA8cBqpb2FxUJECl36fhcvS3AkjDuo5QsCrtSn1+lbjYJIA
 algod_client = algod.AlgodClient(algod_token, algod_address_testnet)
 indexer_client = indexer.IndexerClient(indexer_token, indexer_address_testnet)
 
-def create_nft(name, symbol, metadata_url, reserve="", freeze="", clawback=""):
+def verify_opt_in(address, asset_id):
+  account_info = indexer_client.lookup_account_assets(address=address, asset_id=asset_id)
+  
+  if account_info.get("assets")[0].get("asset-id") == asset_id:
+    return True
+  else:
+    return False
+
+def claim_nft(address, asset_id):
+  
+  if (verify_opt_in(address=address, asset_id=asset_id)) == False:
+    print("Asset with Id: {} has not yet been opted into by Wallet: {}. Please opt-in").format(asset_id, address)
+    return False
+   
+  sp = algod_client.suggested_params()
+ 
+  txn = transaction.AssetTransferTxn(
+    sender=admin_address,
+    sp=sp,
+    receiver=address,
+    amt=1,
+    index=asset_id  
+  )
+ 
+  signed_txn = txn.sign(admin_key)
+
+  txid = algod_client.send_transaction(signed_txn)
+  print("Successfully sent transaction with txID: {}".format(txid))
+
+  try:
+    confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
+
+  except Exception as err:
+    print(err)
+    return False
+
+  print("Transaction information: {}".format(json.dumps(confirmed_txn, indent=4)))
+
+  return txid
+
+# make function asynchronous
+def verify_opt_in_before_transfer(address, asset):
+  time.sleep(150)
+  claim_nft(address, asset)
+
+def create_account():
+  private_key, address = account.generate_account()
+  print("Please store all account information safely. You can import the mnemonic in a Pera Algo wallet or a MyAlgo wallet")
+  print("My address: {}".format(address))
+  print("My private key: {}".format(private_key))
+  print("My passphrase: {}".format(mnemonic.from_private_key(private_key)))
+  print("Please fund the wallet from the algo faucet at https://bank.testnet.algorand.network/")
+  return address
+
+def create_nft(name, symbol, metadata_url, address="", reserve="", freeze="", clawback=""):
   """
   returns the id of the created NFT
   """
+  if address == "":
+    address = create_account()
+
   metadata_hash = hashlib.new("sha256", metadata_url.encode())
   metadata_bytes = metadata_hash.digest()
   
@@ -62,55 +120,12 @@ def create_nft(name, symbol, metadata_url, reserve="", freeze="", clawback=""):
 
   print("Asset ID: {}".format(confirmed_txn["asset-index"]))
 
+  print("Please opt-in to asset on your wallet by adding the Asset Id")
+
+  verify_opt_in_before_transfer(address, confirmed_txn["asset-index"])
+
   return confirmed_txn["asset-index"]
  
-def verify_opt_in(address, asset_id):
-  account_info = indexer_client.lookup_account_assets(address=address, asset_id=asset_id)
-  
-  if account_info.get("assets")[0].get("asset-id") == asset_id:
-    return True
-  else:
-    return False
- 
-def claim_nft(address, asset_id):
-  
-  if (verify_opt_in(address=address, asset_id=asset_id)) == False:
-    print("Asset with Id: {} has not yet been opted into by Wallet: {}. Please opt-in and try again").format(asset_id, address)
-    return False
-   
-  sp = algod_client.suggested_params()
- 
-  txn = transaction.AssetTransferTxn(
-    sender=admin_address,
-    sp=sp,
-    receiver=address,
-    amt=1,
-    index=asset_id  
-  )
- 
-  signed_txn = txn.sign(admin_key)
-
-  txid = algod_client.send_transaction(signed_txn)
-  print("Successfully sent transaction with txID: {}".format(txid))
-
-  try:
-    confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)
-
-  except Exception as err:
-    print(err)
-    return False
-
-  print("Transaction information: {}".format(json.dumps(confirmed_txn, indent=4)))
-
-  return txid
- 
-def create_account():
-  private_key, address = account.generate_account()
-  print("My address: {}".format(address))
-  print("My private key: {}".format(private_key))
-  print("My passphrase: {}".format(mnemonic.from_private_key(private_key)))
-  
-  return address
  
 def update_nft(asset_id, reserve="", clawback="", freeze=""):
   sp = algod_client.suggested_params()
@@ -146,7 +161,4 @@ def update_nft(asset_id, reserve="", clawback="", freeze=""):
   print("Asset ID: {}".format(confirmed_txn["asset-index"]))
 
   return confirmed_txn["asset-index"]
- 
- 
- 
- 
+
