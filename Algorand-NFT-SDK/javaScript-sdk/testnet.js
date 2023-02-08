36948ff8a0ec90f7dacb4429ad524e813836c544
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2023-02-08 11:31:32 +0100

    Update testnet.js

--- Algorand-NFT-SDK/javaScript-sdk/testnet.js
+++ Algorand-NFT-SDK/javaScript-sdk/testnet.js
@@ -10,12 +10,13 @@ const algodBetanetServer = 'https://betanet-api.algonode.cloud'
 const indexerBetanetServer = 'https://betanet-idx.algonode.cloud'
 const algodMainetServer = 'https://mainnet-api.algonode.cloud'
 const indexerMainnetServer = 'https://mainnet-idx.algonode.cloud'
-const algodPort = 443;
-const indexerPort = 443;
+const algodPort = 443
+const indexerPort = 443
 const algodToken = ''
 const indexerToken = ''
 
-const adminAddress = 'W4BERQ52RZILAKXNJJ6X5FNY3ASIAK3OV6KWX7DRTLKHXE7HNNGCO5OVUA'
+const adminAddress =
+	'W4BERQ52RZILAKXNJJ6X5FNY3ASIAK3OV6KWX7DRTLKHXE7HNNGCO5OVUA'
 const adminKey =
 	'3d43u0uywHWdackLRDwgZA8cBqpb2FxUJECl36fhcvS3AkjDuo5QsCrtSn1+lbjYJIArbq+Va/xxmtR7k+drTA=='
 
@@ -44,54 +45,61 @@ const createAccount = function () {
 
 		return myaccount
 	} catch (err) {
-		console.log({err})
+		console.log({ err })
 	}
 }
 
 async function checkOptIn(address, assetId) {
-    const accountInfo = await tokenClient.accountInformation(address).do();
-    const assets = accountInfo.assets;
+	const accountInfo = await tokenClient.accountInformation(address).do()
+	const assets = accountInfo.assets
 
-    let optInStatus = false;
-    for (const asset of assets) {
-        if (asset.asset_id === parseInt(assetId)) {
-            optInStatus = true;
-            break;
-        }
-    }
-	return optInStatus;
-
+	let optInStatus = false
+	for (const asset of assets) {
+		if (asset.asset_id === parseInt(assetId)) {
+			optInStatus = true
+			break
+		}
+	}
+	return optInStatus
 }
 
 async function claimNFT(address, assetId) {
-	const isOptIn = await checkOptIn(address, assetId);
+	const isOptIn = await checkOptIn(address, assetId)
 	if (!isOptIn) {
-	  console.log(`Asset with Id: ${assetId} has not yet been opted into by Wallet: ${address}. Please opt-in`);
-	  return false;
+		console.log(
+			`Asset with Id: ${assetId} has not yet been opted into by Wallet: ${address}. Please opt-in`
+		)
+		return false
 	}
-  
-	const params = algodClient.getTransactionParams();
+
+	const params = algodClient.getTransactionParams()
 	const txn = algosdk.makeAssetTransferTxn(
-	  address, 
-	  [{assetId: assetId, amount: 1}], 
-	  params
-	);
-	const signedTxn = txn.sign(adminKey);
-	const tx = await algodClient.sendRawTransaction(signedTxn).do();
-	console.log(`Successfully sent transaction with txID: ${tx.txId}`);
-  
-	const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
-	console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
-	console.log(`Transaction ID: ${tx.txId}`);
-  
-	return tx.txId;
+		address,
+		[{ assetId: assetId, amount: 1 }],
+		params
+	)
+	const signedTxn = txn.sign(adminKey)
+	const tx = await algodClient.sendRawTransaction(signedTxn).do()
+	console.log(`Successfully sent transaction with txID: ${tx.txId}`)
+
+	const confirmedTxn = await algosdk.waitForConfirmation(
+		algodClient,
+		tx.txId,
+		10
+	)
+	console.log(
+		`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`
+	)
+	console.log(`Transaction ID: ${tx.txId}`)
+
+	return tx.txId
 }
 
 const verifyOptInBeforeTransfer = async (address, asset) => {
 	new Promise((resolve) => {
-		let waiter = undefined;
+		let waiter = undefined
 		waiter = setInterval(async () => {
-			const hasOptIn = await checkOptIn(address, asset); // true || false
+			const hasOptIn = await checkOptIn(address, asset) // true || false
 			if (hasOptIn) {
 				clearInterval(waiter)
 				waiter = undefined
@@ -100,10 +108,10 @@ const verifyOptInBeforeTransfer = async (address, asset) => {
 		}, 5000)
 	})
 		.then(async () => {
-			await claimNFT(address, asset);
+			await claimNFT(address, asset)
 		})
 		.catch((error) => {
-			console.log({error});
+			console.log({ error })
 		})
 }
 
@@ -112,12 +120,12 @@ async function createNft({
 	symbol = undefined,
 	address = undefined,
 	url = undefined,
-	clawback = "",
-	freeze = ""
+	clawback = '',
+	freeze = '',
 } = {}) {
-	if (name === undefined || symbol === undefined || url === undefined){
-		console.log("Argument missing required parameter");
-		return;
+	if (name === undefined || symbol === undefined || url === undefined) {
+		console.log('Argument missing required parameter')
+		return
 	}
 
 	if (address === undefined) {
@@ -127,16 +135,15 @@ async function createNft({
 	const sp = await algodClient.getTransactionParams().do()
 
 	// todo create if statement to check if reserve and if reserve, build metadata hash else empty string
-	const hash = crypto.createHash('sha256');
+	const hash = crypto.createHash('sha256')
 	hash.update(url)
 	const digest = hash.digest()
-	const hexDigest = digest.toString('hex');
- console.log(`Your metadata hash: ${hexDigest}`);
-
+	const hexDigest = digest.toString('hex')
+	console.log(`Your metadata hash: ${hexDigest}`)
 
 	const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
 		// todo may change url p
-		assetMetadataHash: "",
+		assetMetadataHash: digest,
 		assetName: name,
 		assetURL: url,
 		clawback: clawback,
@@ -145,7 +152,7 @@ async function createNft({
 		freeze: freeze,
 		from: adminAddress,
 		manager: adminAddress,
-		reserve: "",
+		reserve: undefined,
 		suggestedParams: sp,
 		total: 1,
 		unitName: symbol,
@@ -157,72 +164,80 @@ async function createNft({
 	console.log('Transaction : ' + tx.txId)
 	let assetID = null
 	// wait for transaction to be confirmed
-	
-	try{
-	const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
 
-	// Get the new asset's information from the creator account
-	let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
-	assetID = ptx['asset-index']
+	try {
+		const confirmedTxn = await algosdk.waitForConfirmation(
+			algodClient,
+			tx.txId,
+			10
+		)
 
-	console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
-    console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`);
+		// Get the new asset's information from the creator account
+		let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
+		assetID = ptx['asset-index']
 
+		console.log(
+			`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`
+		)
+		console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`)
 	} catch (err) {
-    console.error({err});
-    return false;
- 	}
-	verifyOptInBeforeTransfer(address, assetID);
-
+		console.error({ err })
+		return false
+	}
+	verifyOptInBeforeTransfer(address, assetID)
 }
 
 // todo update nft function
-async function updateNFT({assetId = undefined, reserve = undefined, clawback = "", freeze = ""} = {}) {
-
-	if (assetId === undefined || reserve === undefined){
-		console.log("Argument missing required parameter");
+async function updateNFT({
+	assetId = undefined,
+	reserve = undefined,
+	clawback = '',
+	freeze = '',
+} = {}) {
+	if (assetId === undefined || reserve === undefined) {
+		console.log('Argument missing required parameter')
 		return
 	}
 
-    const suggestedParams = await algosdk.algod.getTransactionParams();
-    
-    const txn = algosdk.makeAssetConfigTransaction(
-      adminAddress,
-      {
-        index: assetId,
-        manager: adminAddress,
-        reserve: reserve,
-        freeze: freeze,
-        clawback: clawback,
-        defaultFrozen: false
-      },
-      suggestedParams
-    );
-    
-    const rawSignedTxn = txn.signTxn(adminKey)
+	const suggestedParams = await algosdk.algod.getTransactionParams()
 
+	const txn = algosdk.makeAssetConfigTransaction(
+		adminAddress,
+		{
+			index: assetId,
+			manager: adminAddress,
+			reserve: reserve,
+			freeze: freeze,
+			clawback: clawback,
+			defaultFrozen: false,
+		},
+		suggestedParams
+	)
 
-    let tx = await algodClient.sendRawTransaction(rawSignedTxn).do()
-    console.log('Transaction : ' + tx.txId)
-    let assetID = null
-    // wait for transaction to be confirmed
-   
-    try{
-    const confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 10);
+	const rawSignedTxn = txn.signTxn(adminKey)
 
+	let tx = await algodClient.sendRawTransaction(rawSignedTxn).do()
+	console.log('Transaction : ' + tx.txId)
+	let assetID = null
+	// wait for transaction to be confirmed
 
-    // Get the new asset's information from the creator account
-    let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
-    assetID = ptx['asset-index']
+	try {
+		const confirmedTxn = await algosdk.waitForConfirmation(
+			algodClient,
+			tx.txId,
+			10
+		)
 
+		// Get the new asset's information from the creator account
+		let ptx = await algodClient.pendingTransactionInformation(tx.txId).do()
+		assetID = ptx['asset-index']
 
-    console.log(`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`);
-    console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`);
-
-
-    } catch (err) {
-    console.error(err);
-    return false;
-    }
+		console.log(
+			`Transaction information: ${JSON.stringify(confirmedTxn, null, 4)}`
+		)
+		console.log(`Asset ID: ${confirmedTxn.txresults.createdasset}`)
+	} catch (err) {
+		console.error(err)
+		return false
+	}
 }
-
